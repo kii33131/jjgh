bdec3b40a547e3b9832ddfc88c8141417cdccce0
huiting <33136366@qq.com>
2019-09-01 18:57:37 +0800

    init

--- application/api/controller/Message.php
+++ application/api/controller/Message.php
@@ -9,6 +9,10 @@ use think\Request;
 class Message extends Base
 {
     public function getListByCategory(){
+
+        header('Access-Control-Allow-Origin:*');
+        header('Access-Control-Allow-Methods:OPTIONS, GET, POST'); // 允许option，get，post请求
+        header('Access-Control-Allow-Headers:x-requested-with');
         $data = $this->data_arr;
         $model = new \app\admin\model\Message();
         $list=$model->getListByCategory($data,$this->listRows);
@@ -16,6 +20,9 @@ class Message extends Base
     }
 
     public function detail(){
+        header('Access-Control-Allow-Origin:*');
+        header('Access-Control-Allow-Methods:OPTIONS, GET, POST'); // 允许option，get，post请求
+        header('Access-Control-Allow-Headers:x-requested-with');
         $data = $this->data_arr;
         $model = new \app\admin\model\Message();
         $list=$model->detail($data);
