92fde96f4cd24843f3ed0fbb578126ac19ce25d1
huiting <33136366@qq.com>
2019-09-01 17:43:35 +0800

    init

--- application/api/controller/Message.php
+++ application/api/controller/Message.php
@@ -8,12 +8,12 @@ use think\Request;
 
 class Message extends Base
 {
-
     public function getListByCategory(){
-
         $data = $this->data_arr;
         $model = new \app\admin\model\Message();
         $list=$model->getListByCategory($data,$this->listRows);
         success($list);
     }
+
+    //public function
 }
